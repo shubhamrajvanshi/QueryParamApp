0d702a7688e1ca870d8890bfeda5ef64c1fa8d10
Shubham Rajvanshi <shubhamrajvanshi9@gmail.com>
2020-02-26 21:13:55 -0800

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo yum install gcc-c++ make
+sudo yum install -y gcc-c++ make
 curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
 sudo yum install -y nodejs
 
