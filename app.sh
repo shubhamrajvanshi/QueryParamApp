3436bf4d691f4aba8eabee183b1e5d862d4f1eae
Shubham Rajvanshi <shubhamrajvanshi9@gmail.com>
2020-02-26 22:28:20 -0800

    changed the app reference to nodeapp

--- app.sh
+++ app.sh
@@ -4,6 +4,6 @@ sudo yum install -y gcc-c++ make
 curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
 sudo yum install -y nodejs
 
-git clone https://github.com/shubhamrajvanshi/QueryParamApp.git app
-cd app
+git clone https://github.com/shubhamrajvanshi/QueryParamApp.git nodeapp
+cd nodeapp
 npm install && npm start
