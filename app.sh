0de224f61bc7e299bebb59568f29b9d6040d1c7f
Shubham Rajvanshi <shubhamrajvanshi9@gmail.com>
2020-02-26 22:58:17 -0800

    Changing the sleep timer for node
    Changing the sleep timer for nodejs from 20 to 8s.

--- app.sh
+++ app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo yum install -y gcc-c++ make
-curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
+curl --silent --location https://rpm.nodesource.com/setup_8.x |  sed 's/20/8/g' | sudo bash -
 sudo yum install -y nodejs
 
 git clone https://github.com/shubhamrajvanshi/QueryParamApp.git nodeapp
