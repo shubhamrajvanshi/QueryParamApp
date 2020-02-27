85301f2abca582cbc8db8f175bc788968290fc08
shubhamrajvanshi <shubhamrajvanshi9@gmail.com>
2020-02-26 20:31:51 -0800

    Updated console msg

--- app.js
+++ app.js
@@ -20,11 +20,9 @@ function checkParams(array, queryObject){
   return true;
 }
 
-
 http.createServer(function (req, res) {
   const queryObject = url.parse(req.url,true).query;
-  console.log("object passed is ", queryObject);
-
+  
   if(_.isEmpty(queryObject)){
     console.log("No query strings passed")
     res.writeHead(404,{'Content-type': 'application/json'});
@@ -33,11 +31,13 @@ http.createServer(function (req, res) {
     // res.end('Please send proper request');	
   }
   else if (checkParams(queryParams, queryObject)){
+    console.log("object passed is ", queryObject);
     res.writeHead(200, {'Content-Type': 'application/json'});
     res.write(JSON.stringify(queryObject)+'\n');
     res.end('Feel free to add query parameters to the end of the url');
   }
   else{
+    console.log('Invalid params passed in query params ', queryObject);
     res.writeHead(404,{'Content-Type': 'application/json'});
     res.end('404 Not found');
   }
